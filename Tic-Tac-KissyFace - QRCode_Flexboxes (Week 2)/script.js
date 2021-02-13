501a256696c3b39cf0a06f7c52115d5114e2fb4d
Martin Martin <54905941+gfm262@users.noreply.github.com>
2021-02-13 18:00:38 -0500

    Uploading Tic-Tac-KissyFace Browser-Based Game
    On center square, clicking 4 times will give QR code as a surprise.

---
+++ Tic-Tac-KissyFace - QRCode_Flexboxes (Week 2)/script.js
@@ -0,0 +1,181 @@
+var squareVal1 = 0;
+var squareVal2 = 0;
+var squareVal3 = 0;
+
+var squareVal4 = 0;
+var squareVal5 = 0;
+var squareVal6 = 0;
+
+var squareVal7 = 0;
+var squareVal8 = 0;
+var squareVal9 = 0;
+
+
+//document.addEventListener('click', exOhNo, false);
+
+// var box2 = document.getElementById("r1c2").innerHTML;
+// var box3 = document.getElementById("r1c3").innerHTML;
+//
+// var box4 = document.getElementById("r2c1").innerHTML;
+// var box5 = document.getElementById("r2c2").innerHTML;
+// var box6 = document.getElementById("r2c3").innerHTML;
+//
+// var box7 = document.getElementById("r3c1").innerHTML;
+// var box8 = document.getElementById("r3c2").innerHTML;
+// var box9 = document.getElementById("r3c3").innerHTML;
+
+
+  function exOhNo1(){
+  document.getElementById("r1c1").innerHTML;
+
+  if (squareVal1 == 0){
+    document.getElementById("r1c1").innerHTML = "X";
+  } else if (squareVal1 == 1){
+   document.getElementById("r1c1").innerHTML = "O";
+ } else if (squareVal1 == 2){
+   document.getElementById("r1c1").innerHTML = "😘";
+  }
+
+  squareVal1 += 1;
+  if (squareVal1 == 3){
+    squareVal1 = 0;
+  }
+  }
+
+function exOhNo2(){
+document.getElementById("r1c2").innerHTML;
+
+if (squareVal2 == 0){
+  document.getElementById("r1c2").innerHTML = "X";
+} else if (squareVal2 == 1){
+ document.getElementById("r1c2").innerHTML = "O";
+} else if (squareVal2 == 2){
+ document.getElementById("r1c2").innerHTML = "😘";
+}
+
+squareVal2 += 1;
+if (squareVal2 == 3){
+  squareVal2 = 0;
+}
+}
+
+function exOhNo3(){
+document.getElementById("r1c3").innerHTML;
+
+if (squareVal3 == 0){
+  document.getElementById("r1c3").innerHTML = "X";
+} else if (squareVal3 == 1){
+ document.getElementById("r1c3").innerHTML = "O";
+} else if (squareVal3 == 2){
+ document.getElementById("r1c3").innerHTML = "😘";
+}
+
+squareVal3 += 1;
+if (squareVal3 == 3){
+  squareVal3 = 0;
+}
+}
+
+function exOhNo4(){
+document.getElementById("r2c1").innerHTML;
+
+if (squareVal4 == 0){
+  document.getElementById("r2c1").innerHTML = "X";
+} else if (squareVal4 == 1){
+ document.getElementById("r2c1").innerHTML = "O";
+} else if (squareVal4 == 2){
+ document.getElementById("r2c1").innerHTML = "😘";
+}
+
+squareVal4 += 1;
+if (squareVal4 == 3){
+  squareVal4 = 0;
+}
+}
+
+function exOhNo5(){
+document.getElementById("r2c2").innerHTML;
+
+if (squareVal5 == 0){
+  document.getElementById("r2c2").innerHTML = "X";
+} else if (squareVal5 == 1){
+ document.getElementById("r2c2").innerHTML = "O";
+} else if (squareVal5 == 2){
+ document.getElementById("r2c2").innerHTML = '😘';
+} else if (squareVal5 == 3){
+ document.getElementById("r2c2").innerHTML = '<div id="code">Play on your mobile device<br/><img id="codeb" src="xo_code.png" alt="Image of a QR Code"></div>';
+}
+
+squareVal5 += 1;
+if (squareVal5 == 4){
+  squareVal5 = 0;
+}
+}
+
+function exOhNo6(){
+document.getElementById("r2c3").innerHTML;
+
+if (squareVal6 == 0){
+  document.getElementById("r2c3").innerHTML = "X";
+} else if (squareVal6 == 1){
+ document.getElementById("r2c3").innerHTML = "O";
+} else if (squareVal6 == 2){
+ document.getElementById("r2c3").innerHTML = "😘";
+}
+
+squareVal6 += 1;
+if (squareVal6 == 3){
+  squareVal6 = 0;
+}
+}
+
+function exOhNo7(){
+document.getElementById("r3c1").innerHTML;
+
+if (squareVal7 == 0){
+  document.getElementById("r3c1").innerHTML = "X";
+} else if (squareVal7 == 1){
+ document.getElementById("r3c1").innerHTML = "O";
+} else if (squareVal7 == 2){
+ document.getElementById("r3c1").innerHTML = "😘";
+}
+
+squareVal7 += 1;
+if (squareVal7 == 3){
+  squareVal7 = 0;
+}
+}
+
+function exOhNo8(){
+document.getElementById("r3c2").innerHTML;
+
+if (squareVal8 == 0){
+  document.getElementById("r3c2").innerHTML = "X";
+} else if (squareVal8 == 1){
+ document.getElementById("r3c2").innerHTML = "O";
+} else if (squareVal8 == 2){
+ document.getElementById("r3c2").innerHTML = "😘";
+}
+
+squareVal8 += 1;
+if (squareVal8 == 3){
+  squareVal8 = 0;
+}
+}
+
+function exOhNo9(){
+document.getElementById("r3c3").innerHTML;
+
+if (squareVal9 == 0){
+  document.getElementById("r3c3").innerHTML = "X";
+} else if (squareVal9 == 1){
+ document.getElementById("r3c3").innerHTML = "O";
+} else if (squareVal9 == 2){
+ document.getElementById("r3c3").innerHTML = "😘";
+}
+
+squareVal9 += 1;
+if (squareVal9 == 3){
+  squareVal9 = 0;
+}
+}
